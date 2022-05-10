c6ee251b1c5b8def81ffb7f1c3deb26c81abc868
Your Name <email@example.com>
2022-05-10 22:35:58 +0800

    golang time-1

---
+++ go-by-example-master/proxy/v4/main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const socks5Ver = 0x05
+const cmdBind = 0x01
+const atypIPV4 = 0x01
+const atypeHOST = 0x03
+const atypeIPV6 = 0x04
+
+func main() {
+	server, err := net.Listen("tcp", "127.0.0.1:1080") // 监听该端口，若监听有请求则开始处理
+	if err != nil {
+		panic(err)
+	}
+	for {
+		client, err := server.Accept() // 建立客户端与代理服务器连接
+		if err != nil {
+			log.Printf("Accept failed %v", err)
+			continue
+		}
+		go process(client) // 调用process处理该链接
+	}
+}
+
+func process(conn net.Conn) {
+	defer conn.Close() // 程序运行结束关闭连接
+	reader := bufio.NewReader(conn)
+	err := auth(reader, conn) // 1.认证阶段（客户端与代理服务器之间进行认证）
+	if err != nil {
+		log.Printf("client %v auth failed:%v", conn.RemoteAddr(), err)
+		return
+	}
+	err = connect(reader, conn) // 2.建立连接（代理服务器根据用户的目的ip建立和底层服务器的连接）
+	if err != nil {
+		log.Printf("client %v auth failed:%v", conn.RemoteAddr(), err)
+		return
+	}
+}
+
+func auth(reader *bufio.Reader, conn net.Conn) (err error) {
+	// 代理服务器接收用户发送的认证报文，解读其中信息
+	// +----+----------+----------+
+	// |VER | NMETHODS | METHODS  |
+	// +----+----------+----------+
+	// | 1  |    1     | 1 to 255 |
+	// +----+----------+----------+
+	// VER: 协议版本，socks5为0x05
+	// NMETHODS: 支持认证的方法数量
+	// METHODS: 对应NMETHODS，NMETHODS的值为多少，METHODS就有多少个字节。RFC预定义了一些值的含义，内容如下:
+	// X’00’ NO AUTHENTICATION REQUIRED
+	// X’02’ USERNAME/PASSWORD
+
+	ver, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read ver failed:%w", err)
+	}
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	methodSize, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read methodSize failed:%w", err)
+	}
+	method := make([]byte, methodSize)
+	_, err = io.ReadFull(reader, method)
+	if err != nil {
+		return fmt.Errorf("read method failed:%w", err)
+	}
+
+	// 代理服务器返回给用户的报文
+	// +----+--------+
+	// |VER | METHOD |
+	// +----+--------+
+	// | 1  |   1    |
+	// +----+--------+
+	_, err = conn.Write([]byte{socks5Ver, 0x00})
+	if err != nil {
+		return fmt.Errorf("write failed:%w", err)
+	}
+	return nil
+}
+
+func connect(reader *bufio.Reader, conn net.Conn) (err error) {
+	// 代理服务器接收用户端发送的请求连接的报文
+	// +----+-----+-------+------+----------+----------+
+	// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER 版本号，socks5的值为0x05
+	// CMD 0x01表示CONNECT请求
+	// RSV 保留字段，值为0x00
+	// ATYP 目标地址类型，DST.ADDR的数据对应这个字段的类型。
+	//   0x01表示IPv4地址，DST.ADDR为4个字节
+	//   0x03表示域名，DST.ADDR是一个可变长度的域名
+	// DST.ADDR 一个可变长度的值
+	// DST.PORT 目标端口，固定2个字节
+
+	buf := make([]byte, 4)
+	_, err = io.ReadFull(reader, buf)
+	if err != nil {
+		return fmt.Errorf("read header failed:%w", err)
+	}
+	ver, cmd, atyp := buf[0], buf[1], buf[3]
+	if ver != socks5Ver {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	if cmd != cmdBind {
+		return fmt.Errorf("not supported cmd:%v", ver)
+	}
+	addr := ""
+	switch atyp {
+	case atypIPV4:
+		_, err = io.ReadFull(reader, buf)
+		if err != nil {
+			return fmt.Errorf("read atyp failed:%w", err)
+		}
+		addr = fmt.Sprintf("%d.%d.%d.%d", buf[0], buf[1], buf[2], buf[3])
+	case atypeHOST:
+		hostSize, err := reader.ReadByte()
+		if err != nil {
+			return fmt.Errorf("read hostSize failed:%w", err)
+		}
+		host := make([]byte, hostSize)
+		_, err = io.ReadFull(reader, host)
+		if err != nil {
+			return fmt.Errorf("read host failed:%w", err)
+		}
+		addr = string(host)
+	case atypeIPV6:
+		return errors.New("IPv6: no supported yet")
+	default:
+		return errors.New("invalid atyp")
+	}
+	_, err = io.ReadFull(reader, buf[:2])
+	if err != nil {
+		return fmt.Errorf("read port failed:%w", err)
+	}
+	port := binary.BigEndian.Uint16(buf[:2])
+
+	// 用户请求连接报文无误，代理服务器建立到底层服务器的连接
+	dest, err := net.Dial("tcp", fmt.Sprintf("%v:%v", addr, port))
+	if err != nil {
+		return fmt.Errorf("dial dst failed:%w", err)
+	}
+	defer dest.Close()
+	log.Println("dial", addr, port)
+
+	// 代理服务器给用户端的回复报文，回复用户的请求连接报文
+	// +----+-----+-------+------+----------+----------+
+	// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER socks版本，这里为0x05
+	// REP Relay field,内容取值如下 X’00’ succeeded
+	// RSV 保留字段
+	// ATYPE 地址类型
+	// BND.ADDR 服务绑定的地址
+	// BND.PORT 服务绑定的端口DST.PORT
+	_, err = conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0})
+	if err != nil {
+		return fmt.Errorf("write failed: %w", err)
+	}
+
+	// 保证在数据发送完成前，程序不会结束运行，若其中一个方向不进行数据传输则调用cancel？
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		_, _ = io.Copy(dest, reader) // 将用户数据发送到目的服务器
+		cancel()
+	}()
+	go func() {
+		_, _ = io.Copy(conn, dest) // 将目的服务器数据拷贝至用户端
+		cancel()
+	}()
+
+	<-ctx.Done()
+	return nil
+}
---
+++ go-project-example-0.1/repository/topic.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"sync"
+)
+
+type Topic struct {
+	Id         int64  `json:"id"`
+	Title      string `json:"title"`
+	Content    string `json:"content"`
+	CreateTime int64  `json:"create_time"`
+}
+type TopicDao struct {
+}
+
+var (
+	topicDao  *TopicDao
+	topicOnce sync.Once
+)
+
+// 查询帖子
+func NewTopicDaoInstance() *TopicDao {
+	topicOnce.Do(
+		func() {
+			topicDao = &TopicDao{}
+		})
+	return topicDao
+}
+func (*TopicDao) QueryTopicById(id int64) *Topic {
+	return topicIndexMap[id]
+}
